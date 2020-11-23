97241d4b58b6e91d918cdfd2181a242c6b5d20eb
marcellofdz <marcellofernandez98@gmail.com>
2020-11-23 03:06:32 -0400

    Code 100%

---
+++ ejercicio11.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Exercise11 
+{
+	public static void main(String[] args)
+	{
+		int vNum1 = Integer.parseInt(args[0]);
+		
+		 if ( (vNum1 == 0) || (vNum1 < 0))
+		{
+		     System.out.println("Invalid input. Don't use "+ vNum1+ ". Try Again");
+		     System.exit(2);
+		}
+
+		
+
+		else if (vNum1 > 0) 
+		{
+
+	             System.out.println("");
+	             for(int i = 1; i < (vNum1+1); i++)
+		  
+		  {  
+
+		       System.out.println(i);
+
+		  }   
+
+                System.out.println("");
+
+                } 
+		
+		else {
+	                    System.out.println("\nInvalid input [" + vNum1 + "].\n");
+	             }		    
+			
+	
+	}
+
+
+
+
+
+}
