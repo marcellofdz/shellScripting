205631b7d039e995e233b5a01c8e6a371fa2a130
marcellofdz <marcellofernandez98@gmail.com>
2020-11-23 01:13:05 -0400

    Code functional using the arguments on java

---
+++ ejercicio6.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Exercise6 
+
+{
+
+	public static void main(String[] args) 
+	
+	{	
+		int vNum1 = Integer.parseInt(args[0]);
+		if ((vNum1 == 0))
+		{
+			System.out.println("Invalid input. Don't use 0. Try Again");
+			System.exit(2);
+		}
+
+		
+		else if ((vNum1 < 0)) 
+		{	
+			System.out.println("Invalid input. Don't use negative numbers. Try Again");
+			System.exit(2);
+		}
+		else if (vNum1 > 0)
+		{	
+			System.out.println("Good One [" + vNum1 + "].");
+		
+		}
+
+		else 
+		{
+			System.out.println("Damn Bro, check this code, for some reason you have a error that you and I dont know.");
+		}
+}
+}
+//endLn
