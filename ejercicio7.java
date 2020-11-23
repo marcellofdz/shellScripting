831d875c2191d5b0fbfd86f0c1e0d152050a517c
marcellofdz <marcellofernandez98@gmail.com>
2020-11-23 01:50:01 -0400

    Code functional to exeption on trow error to output.txt file

---
+++ ejercicio7.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+class Exercise7 
+
+{
+
+	public static void main(String[] args) throws FileNotFoundException 
+	
+	{	
+		int vNum1 = Integer.parseInt(args[0]);
+		if ((vNum1 == 0))
+		{
+			System.out.println("Invalid input. Don't use 0. Try Again");
+			System.exit(2);
+		}
+
+		
+		else if ((vNum1 < 0)) 
+		{	
+			System.out.println("Invalid input. Don't use negative numbers ["+ vNum1 +"]. Try Again");
+			System.exit(2);
+
+		}
+		else if (vNum1 > 0)
+		{	
+			
+	                File output = new File("out.txt");
+	                FileOutputStream fos = new FileOutputStream(output);
+	                PrintStream ps = new PrintStream(fos);
+	                System.setOut(ps);
+			System.out.println("Good One [" + vNum1 + "].");
+		
+		}
+
+		else 
+		{
+			System.out.println("Damn Bro, check this code, for some reason you have a error that you and I dont know.");
+		}
+}
+}
+//endLn
